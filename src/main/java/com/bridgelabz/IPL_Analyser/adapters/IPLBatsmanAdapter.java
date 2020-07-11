1a2adc069934fc419b68600bd47ab017496f52f3
PrajyotGajane <prajyotgajane@gmail.com>
2020-07-12 01:10:01 +0530

    Added: Batsman Adapter class to load IPL data

--- src/main/java/com/bridgelabz/IPL_Analyser/adapters/IPLBatsmanAdapter.java
+++ src/main/java/com/bridgelabz/IPL_Analyser/adapters/IPLBatsmanAdapter.java
@@ -1,11 +1,15 @@
 package com.bridgelabz.IPL_Analyser.adapters;
 
+import com.bridgelabz.IPL_Analyser.exception.IPLAnalyserException;
+import com.bridgelabz.IPL_Analyser.models.IPLBatsmanCSV;
+import com.bridgelabz.IPL_Analyser.models.IPLBowlerCSV;
 import com.bridgelabz.IPL_Analyser.models.IPLDAO;
+import com.bridgelabz.IPL_Analyser.service.CSVBuilderException;
 
 import java.util.Map;
 
-public class IPLBatsmanAdapter {
-      public Map<String, IPLDAO> loadIPLData(String[] csvFilePath) {
-            return null;
+public class IPLBatsmanAdapter extends IPLAdaptor{
+      public Map<String, IPLDAO> loadIPLData(String... csvFilePath) throws CSVBuilderException, IPLAnalyserException {
+            return super.loadIPLData(IPLBatsmanCSV.class, csvFilePath[0]);
       }
 }
